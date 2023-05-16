b43839eb6770c6871fdcbab0cc842cac17ae6eab
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-05-16 10:39:36 +0200

    Add code to go directly from VEP to HGVS-like notation

---
+++ scripts/test_vep_table.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import pytest
+from vep_table import hgvs_like
+from collections import namedtuple
+
+Variant = namedtuple("Variant", "chrom start end variant_class")
+
+
+VARIANTS = [
+        # Small insertion
+        ("chr1", 114708595, 114708594, "insertion", "-/T", "NC_000001.11:g.114708594_114708595insT"),
+        # Big insertion
+        ("chr5", 171410540, 171410539, "insertion", "-/TCTG", "NC_000005.10:g.171410539_171410540insTCTG"),
+        # SNP
+        ("chr1", 114716123, 114716123, "SNV", "C/T", "NC_000001.11:g.114716123C>T"),
+        # Small deletion
+        ("chr11", 32435164, 32435164, "deletion", "G/-", "NC_000011.10:g.32435164_32435164del"),
+        # Big deletion
+        ("chr19", 33301849, 33301857, "deletion", "GGCGGCGGC/-", "NC_000019.10:g.33301849_33301857del"),
+        # Susbstitution
+        ("chr4", 54733129, 54733130, "substitution", "GA/TC", "NC_000004.12:g.54733129_54733130delinsTC"),
+        # Indel, delete two, insert one base
+        ("chr11", 32434944, 32434945, "indel", "GC/T", "NC_000011.10:g.32434944_32434945delinsT"),
+        # Indel, delete three, insert one base
+        ("chr4", 105236254, 105236256, "indel", "GAG/C", "NC_000004.12:g.105236254_105236256delinsC"),
+        # Indel, delete two, insert three bases
+        ("chr19", 33302112, 33302113, "indel", "GC/CGT", "NC_000019.10:g.33302112_33302113delinsCGT"),
+]
+
+NOT_IMPLEMENTED_VARIANTS = [
+    # An insertion where the reference allele is not '-'
+    ("chr1", 114708595, 114708594, "insertion", "T/-", ""),
+    # A SNV where start is not equal to end
+    ("chr1", 114716123, 1, "SNV", "C/T", "NC_000001.11:g.114716123C>T"),
+    # A deletion where the alt allele is not '-'
+    ("chr11", 32435164, 32435164, "deletion", "-/G", ""),
+    # A substitution where ref and alt are of different size
+    ("chr1", 1, 2, "substitution", "G/TC", ""),
+    # Unsupported variant classes
+    ("chr1", 1, 2, "sequence_alteration", "G/TC", ""),
+    ("chr1", 1, 2, "sequence_variation", "G/TC", ""),
+]
+
+
+@pytest.mark.parametrize(["chrom", "start", "end", "variant_class", "allele_string", "hgvs"], VARIANTS)
+def test_hgvs_like(chrom, start, end, variant_class, allele_string, hgvs):
+    assert hgvs_like(chrom, start, end, variant_class, allele_string) == hgvs
+
+
+@pytest.mark.parametrize(["chrom", "start", "end", "variant_class", "allele_string", "hgvs"], NOT_IMPLEMENTED_VARIANTS)
+def test_no_implementedhgvs_like(chrom, start, end, variant_class, allele_string, hgvs):
+    with pytest.raises(NotImplementedError):
+        hgvs_like(chrom, start, end, variant_class, allele_string)
---
+++ scripts/vep_table.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import argparse
+import xopen
+import json
+
+
+FIELDS = [
+    "seq_region_name",
+    "start",
+    "end",
+    "variant_class",
+    "allele_string"
+]
+
+TRANSCRIPT_FIELDS = [
+    "variant_allele",
+    "gene_id",
+    "gene_symbol",
+    "hgvsc",
+    "hgvsp",
+    "impact",
+    "consequence_terms"
+]
+
+MAPPING = {
+    'chr1': 'NC_000001.11',
+    'chr2': 'NC_000002.12',
+    'chr3': 'NC_000003.12',
+    'chr4': 'NC_000004.12',
+    'chr5': 'NC_000005.10',
+    'chr6': 'NC_000006.12',
+    'chr7': 'NC_000007.14',
+    'chr8': 'NC_000008.11',
+    'chr9': 'NC_000009.12',
+    'chr10': 'NC_000010.11',
+    'chr11': 'NC_000011.10',
+    'chr12': 'NC_000012.12',
+    'chr13': 'NC_000013.11',
+    'chr14': 'NC_000014.9',
+    'chr15': 'NC_000015.10',
+    'chr16': 'NC_000016.10',
+    'chr17': 'NC_000017.11',
+    'chr18': 'NC_000018.10',
+    'chr19': 'NC_000019.10',
+    'chr20': 'NC_000020.11',
+    'chr21': 'NC_000021.9',
+    'chr22': 'NC_000022.11',
+    'chrX': 'NC_000023.11',
+    'chrY': 'NC_000024.10',
+    'chrM': 'NC_012920.1'
+}
+
+
+def hgvs_like(chrom, start, end, variant_class, allele_string):
+    """Create a non-normalised HGVS description for a given variant"""
+    chrom = MAPPING[chrom]
+
+    # Variant representation in case of error
+    var = f"{variant_class} {chrom}:{start} {allele_string}"
+
+    # Pull out the ref and alt from the allele_string
+    ref, alt = allele_string.split("/")
+
+    if variant_class == "insertion":
+        if not ref == "-":
+            msg = f"{var} not supported"
+            raise NotImplementedError(msg)
+        return f"{chrom}:g.{end}_{start}ins{alt}"
+
+    if variant_class == "SNV":
+        if start != end:
+            msg = f"{var} not supported"
+            raise NotImplementedError(msg)
+        return f"{chrom}:g.{start}{ref}>{alt}"
+
+    if variant_class == "deletion":
+        if not alt == "-":
+            msg = f"{var} not supported"
+            raise NotImplementedError(msg)
+        return f"{chrom}:g.{start}_{end}del"
+
+    if variant_class == "substitution":
+        if len(ref) != len(alt):
+            msg = f"{var} not supported"
+            raise NotImplementedError(msg)
+        return f"{chrom}:g.{start}_{end}delins{alt}"
+
+    if variant_class == "indel":
+        return f"{chrom}:g.{start}_{end}delins{alt}"
+
+    # Unhandle variant_class
+    msg = f"{var} not supported"
+    raise NotImplementedError(msg)
+
+
+def read_vep(fname):
+    with xopen.xopen(fname) as fin:
+        for line in fin:
+            yield json.loads(line)
+
+
+def main(fnames, sep):
+    print(*FIELDS, *TRANSCRIPT_FIELDS, sep=sep)
+    for vep in fnames:
+        for variant in read_vep(vep):
+            for transcript in variant["transcript_consequences"]:
+                # Get all the data we want to print in a list
+                variant_data = [variant.get(f) for f in FIELDS]
+                transcript_data = [transcript.get(f) for f in TRANSCRIPT_FIELDS]
+                print(*variant_data, *transcript_data, sep=sep)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vep', nargs='+')
+    parser.add_argument('--sep', default='\t')
+
+    args = parser.parse_args()
+
+    main(args.vep, args.sep)
